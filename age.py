07e857c1740abca65b3f6573120334b7eddefbee
Venug2003 <venu4877363@gmail.com>
2023-10-31 13:55:36 +0530

    Add dropdown

--- age.py
+++ age.py
@@ -3,6 +3,7 @@ age=input('Enter person age')
 age=int(age)
 year=2023-age
 print("This is a new branch demo feature")
+print("add dropdown")
 if year>=60:
     print("He is a senior citizen")
 else:
