a654137e0d075b9957e424dbff99afbf919f3808
Venug2003 <venu4877363@gmail.com>
2023-10-31 13:16:37 +0530

    Python age code

---
+++ age.py
@@ -0,0 +1,9 @@
+name=input("Enter person name")
+age=input('Enter person age')
+age=int(age)
+year=2023-age
+if year>=60:
+    print("He is a senior citizen")
+else:
+    print("He is not a senior citizen")
+
